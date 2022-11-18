c6b8fc967d0f771e924e8a7441885fea1c8555a8
Danielle Levy <107309224+DanielleLevy@users.noreply.github.com>
2022-11-18 17:26:45 +0200

    FUNCTION NUM 2

---
+++ CanberraDistance.cpp
@@ -0,0 +1,22 @@
+//
+// Created by danie on 18/11/2022.
+//
+#include <iostream>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+float CanberraD(vector <float> a,  vector <float> b ){
+    /**
+     * the function gets 2 vector in the same size and conclute their's canberra distance
+     */
+    float sum=0; //to sun the distances between the element of the vectors
+    float distance; // to compute the distance between the element of the vector
+    for (int i=0;i<a.size();i++) // runs on the vector's elements
+    {
+        distance=(abs(a[i]-b[i]))/(abs(a[i])+abs(b[i]));
+        sum=sum+distance ;
+    }
+    return sum ;
+}
+
--- ChebyshevDistance .cpp
+++ ChebyshevDistance .cpp
@@ -7,21 +7,19 @@
 #include <cmath>
 using namespace std;
 
-float Chebyshec (vector <float> a,  vector <float> b ){
+float chebyshec (vector <float> a, vector <float> b ){
+    /**
+      * the function gets 2 vector in the same size and conclute their's canberra distance
+     */
     float max;  //use for the loop to find the maximum distance
-    float distance[a.size()];
-    for (int i=0;i<a.size();i++)
+    float distance[a.size()]; // to save the distance between the element of the vector
+    for (int i=0;i<a.size();i++) // runs on the vector's elements
     {
-        distance[i]=abs(a[i]-b[i]);
+        distance[i]=abs(a[i]-b[i]); //compute the distance between the element of the vector
     }
-    max = distance[0] ;
-    for (int j=1;j<a.size();j++)
+    max = distance[0] ; // init the max to the first distance
+    for (int j=1;j<a.size();j++) //find the max distance between the vectors elements
         if (distance[j]>max)
             max=distance[j] ;
     return max ;
-}
-int main(){
-    vector <float> a={1,2,3};
-    vector <float> b={4,90,6};
-    cout<<Chebyshec(a,b)<<endl;
 }
